46e6c2b4c75210c659361167896e2560f3a5afee
BenVonAchen <bennyvonachen@gmail.com>
2022-04-19 11:51:59 -0600

    Added responsive design to images as well as other design changes

--- src/pages/About/about.js
+++ src/pages/About/about.js
@@ -11,9 +11,22 @@ function About() {
           <img src={history} alt="history" />
         </div>
 
-        <p>This is how it all began... 😄</p>
+        <p>
+          Riga, the capital of Latvia, is not only the crown jewel of Latvia,
+          but also of the Baltics. Riga has more than 600,000 inhabitants,
+          making it the largest city in the Baltic States. Riga is also known as
+          an architectural jewel, a city where you can see churches from the
+          city’s origins, medieval buildings in the Old Town, unique examples of
+          Art Nouveau, as well as wooden architecture that has survived the
+          centuries and modern architectural jewels.
+        </p>
 
-        {/* Also include things like fun things to do in Riga, History, Fun Facts, etc...*/}
+        <p>
+          Riga has a beautiful and varied architecture. From Romanesque to
+          Gothic and Baroque,The Old Town has a remarkable number of
+          architectural monuments. A walk through the streets of the Old Town
+          will be an exciting experience for anyone visiting Riga.
+        </p>
       </div>
     </div>
   );
--- src/pages/Gallery/gallery.js
+++ src/pages/Gallery/gallery.js
@@ -9,76 +9,75 @@ import rigasdoms2 from '../../assets/gallery-images/rigasdoms2.png';
 import statue from '../../assets/gallery-images/statue.png';
 import stpete2 from '../../assets/gallery-images/stpete2.png';
 import stpete3 from '../../assets/gallery-images/stpete3.png';
-import streets from '../../assets/gallery-images/streets.png';
 import streets2 from '../../assets/gallery-images/streets2.png';
+import streets3 from '../../assets/gallery-images/streets3.png';
 
 function Gallery() {
   return (
     <div className="container">
       <div id="gallery">
-        <h1>Gallery</h1>
+        <h1>Photo Gallery</h1>
 
         <div className="galleryImages">
           <div className="row">
-            <div className="col-6">
-              <img src={atrnouveau} alt="artnouveau" />
+            <div className="col-sm-6">
+              <img src={statue} alt="statue" />
             </div>
 
-            <div className="col-6">
+            <div className="col-sm-6">
               <img src={blackhouse} alt="blackhouse" />
             </div>
           </div>
-          <br />
-          <div className="row">
-            <div className="col-6">
-              <img src={bridge2} alt="bridge2" />
-            </div>
 
-            <div className="col-6">
-              <img src={statue} alt="statue" />
-            </div>
-          </div>
-          <br />
           <div className="row">
-            <div className="col-6">
-              <img src={building2} alt="building2" />
-            </div>
-
-            <div className="col-6">
-              <img src={rigasdoms} alt="rigasdoms" />
-            </div>
-          </div>
-          <br />
-          <div className="row">
-            <div className="col-6">
-              <img src={rigasdoms2} alt="rigasdoms2" />
-            </div>
-
-            <div className="col-6">
+            <div className="col-sm-6">
               <img src={building} alt="building" />
             </div>
+
+            <div className="col-sm-6">
+              <img src={atrnouveau} alt="artnouveau" />
+            </div>
           </div>
-          <br />
+
           <div className="row">
-            <div className="col-6">
+            <div className="col-sm-6">
+              <img src={rigasdoms} alt="rigasdoms" />
+            </div>
+
+            <div className="col-sm-6">
+              <img src={building2} alt="building2" />
+            </div>
+          </div>
+
+          <div className="row">
+            <div className="col-sm-6">
               <img src={stpete2} alt="stpete2" />
             </div>
 
-            <div className="col-6">
-              <img src={stpete3} alt="stpete3" />
+            <div className="col-sm-6">
+              <img src={rigasdoms2} alt="rigasdoms2" />
             </div>
           </div>
-          <br />
+
           <div className="row">
-            <div className="col-6">
-              <img src={streets} alt="streets" />
+            <div className="col-sm-6">
+              <img src={bridge2} alt="bridge2" />
             </div>
 
-            <div className="col-6">
+            <div className="col-sm-6">
               <img src={streets2} alt="streets2" />
             </div>
           </div>
-          <br />
+
+          <div className="row">
+            <div className="col-sm-6">
+              <img src={streets3} alt="streets3" />
+            </div>
+
+            <div className="col-sm-6">
+              <img src={stpete3} alt="stpete3" />
+            </div>
+          </div>
         </div>
 
         {/* Also include things like fun things to do in Riga, History, Fun Facts, etc...*/}
